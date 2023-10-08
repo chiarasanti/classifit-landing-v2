b069ceffca9662a1b3a584f6297cedf3cecc814c
Chiara Santi <santichiara@hotmail.com>
2023-10-08 22:41:45 +0200

    social preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,11 @@ export default function Home() {
     <>
       <Head>
         <title>Classifit - Sito web su misura e gestionale per il tuo studio di pilates</title>
-        <meta property="og:title" content="Classifit - Sito web su misura e gestionale per il tuo studio di pilates" />
+        <meta property="og:title" content="Porta il tuo studio al prossimo livello" />
         <meta property="og:url" content="https://www.classi.fit/" />
         <meta
           property="og:description"
-          content="Porta il tuo studio al prossimo livello. Con Classifit puoi avere un sito web su misura e un gestionale online per il tuo studio di fitness."
+          content="Sito web su misura e gestionale online integrato - Tutto con un'unica piattaforma"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:image" content="/social-preview.png" />
